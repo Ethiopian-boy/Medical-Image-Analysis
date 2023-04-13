fdf97e3c4a454c8d26983b43c62814cd7e85872b
Kokou Elvis Khorem BLITTI <belviskhoremk@gmail.com>
2023-04-13 15:53:35 +0530

    elvis

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,19 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+import mysql.connector
 
 
 db = SQLAlchemy()
 
 UPLOAD_FOLDER = 'static/uploads/'
-
+cnx = mysql.connector.connect(user='admin', database='ecom', password='Elviskhorem12!?', host='127.0.0.1')
+cursor = cnx.cursor(buffered=True)
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY']  = 'medical image analysis'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://intellify:password@localhost/analysis'#mysql://username:password@server/db
-    db.init_app(app)
-    app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
     
     from .views import views
@@ -24,21 +22,14 @@ def create_app():
     app.register_blueprint(auth, url_prefix=('/'))
     
     from .models import User, Note
-    
-    create_database(app)
+
     
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
     
     return app
 
 
-def create_database(app):
-    with app.app_context():
-        db.create_all()
-        print("Database created! ")
+
--- webapp/views.py
+++ webapp/views.py
@@ -7,6 +7,12 @@ from werkzeug.utils import secure_filename
 import os
 from . import *
 
+from PIL import Image
+import io
+import numpy as np
+import tensorflow as tf
+import cv2
+
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
@@ -97,4 +103,213 @@ def display_image(filename):
 
 #@views.route('/delete-image', methods=['POST'])
 #def delete_image(filename):
-    
+
+@views.route('/pneumonia', methods=['POST', 'GET'])
+def pneumonia():
+    model = tf.keras.models.load_model('/home/belvisk/Documents/models/pneumonia.h5')
+    if request.method == 'POST':
+
+        # Read the file from the request
+        file = request.files['file'].read()
+
+        # Convert the file to an image
+        img = Image.open(io.BytesIO(file))
+        img = img.convert('L')
+
+        # Resize the image to the input size expected by the model
+        img = img.resize((224, 224))
+
+        # Convert the image to a numpy array
+        x = np.array(img)
+        x = np.stack((x,) * 3, axis=-1)
+        x = tf.image.resize(x, [224, 224])
+        x = np.array(x)
+
+        # Normalize the pixel values
+        x = x / 255.0
+
+        # Add a batch dimension to the array
+        x = np.expand_dims(x, axis=0)
+
+        # Make the prediction
+        pred = model.predict(x)
+        threshold = 0.5
+        label = np.where(pred >= threshold, 1, 0)
+        result = int(label)
+        if result == 0:
+            output='The predictions are formal. There is no pneumonia detected'
+        elif result == 1:
+            output='PNEUMONIA detected!!!'
+        return jsonify({'result': output})
+    return render_template('deploy.html')
+
+
+@views.route('/bones', methods=['POST', 'GET'])
+def bones():
+    model = tf.keras.models.load_model('/home/belvisk/Documents/models/bones.h5')
+    if request.method == 'POST':
+
+        # Read the file from the request
+        file = request.files['file'].read()
+
+        # Convert the file to an image
+        img = Image.open(io.BytesIO(file))
+        img = img.convert('L')
+
+        # Resize the image to the input size expected by the model
+        img = img.resize((224, 224))
+
+        # Convert the image to a numpy array
+        x = np.array(img)
+        x = np.stack((x,) * 3, axis=-1)
+        x = tf.image.resize(x, [224, 224])
+        x = np.array(x)
+
+        # Normalize the pixel values
+        x = x / 255.0
+
+        # Add a batch dimension to the array
+        x = np.expand_dims(x, axis=0)
+
+        # Make the prediction
+        pred = model.predict(x)
+        threshold = 0.5
+        label = np.where(pred >= threshold, 1, 0)
+        result = int(label)
+        if result == 0:
+            output='Fracture detected !!!'
+        elif result == 1:
+            output='Everything seems right'
+        return jsonify({'result': output})
+    return render_template('deploy.html')
+
+
+
+@views.route('/tuberculosis', methods=['POST', 'GET'])
+def tuberculosis():
+    model = tf.keras.models.load_model('/home/belvisk/Documents/models/tuberculosis.h5')
+    if request.method == 'POST':
+
+        # Read the file from the request
+        file = request.files['file'].read()
+
+        # Convert the file to an image
+        img = Image.open(io.BytesIO(file))
+        img = img.convert('L')
+
+        # Resize the image to the input size expected by the model
+        img = img.resize((320, 320))
+
+        # Convert the image to a numpy array
+        x = np.array(img)
+        x = np.stack((x,) * 3, axis=-1)
+        x = tf.image.resize(x, [320, 320])
+        x = np.array(x)
+
+        # Normalize the pixel values
+        x = x / 255.0
+
+        # Add a batch dimension to the array
+        x = np.expand_dims(x, axis=0)
+
+        # Make the prediction
+        pred = model.predict(x)
+        threshold = 0.5
+        label = np.where(pred >= threshold, 1, 0)
+        result = int(label)
+        if result == 0:
+            output='Everything seems right'
+        elif result == 1:
+            output='TUBERCULOSIS detected!!!'
+        return jsonify({'result': output})
+    return render_template('deploy.html')
+
+
+
+@views.route('/brainTumor', methods=['POST', 'GET'])
+def brainTumor():
+    model = tf.keras.models.load_model('/home/belvisk/Documents/models/brain-tumor.h5')
+    if request.method == 'POST':
+
+        # Read the file from the request
+        file = request.files['file'].read()
+
+        # Convert the file to an image
+        img = Image.open(io.BytesIO(file))
+        img = img.convert('L')
+
+        # Resize the image to the input size expected by the model
+        img = img.resize((150, 150))
+
+        # Convert the image to a numpy array
+        x = np.array(img)
+        x = np.stack((x,) * 3, axis=-1)
+        x = tf.image.resize(x, [150, 150])
+        x = np.array(x)
+
+        # Normalize the pixel values
+        x = x / 255.0
+
+        # Add a batch dimension to the array
+        x = np.expand_dims(x, axis=0)
+
+        # Make the prediction
+        pred = model.predict(x)
+        label = np.argmax(pred[0])
+        result = label
+        result=int(result)
+        if result == 0:
+            output = 'We think you have a GLIOAMA'
+        elif result==1:
+            output='We think you have a MENINGIOMA'
+        elif result==2:
+            output='No tumor detected'
+        elif result==3:
+            output='We think you have a PITUITARY'
+
+        return jsonify({'result': output})
+    return render_template('deploy.html')
+
+
+
+
+
+
+@views.route('/brainStroke', methods=['POST', 'GET'])
+def brainStroke():
+    model = tf.keras.models.load_model('/home/belvisk/Documents/models/brain_stroke_model.h5')
+    if request.method == 'POST':
+
+        # Read the file from the request
+        file = request.files['file'].read()
+
+        # Convert the file to an image
+        img = Image.open(io.BytesIO(file))
+        img = img.convert('L')
+
+        # Resize the image to the input size expected by the model
+        img = img.resize((224, 224))
+
+        # Convert the image to a numpy array
+        x = np.array(img)
+        x = np.stack((x,) * 3, axis=-1)
+        x = tf.image.resize(x, [224, 224])
+        x = np.array(x)
+
+        # Normalize the pixel values
+        x = x / 255.0
+
+        # Add a batch dimension to the array
+        x = np.expand_dims(x, axis=0)
+
+        # Make the prediction
+        pred = model.predict(x)
+        threshold = 0.5
+        label = np.argmax(pred)
+        result = int(label)
+        if result == 0:
+            output='Everything seems right'
+        elif result == 1:
+            output='brain STroke detected!!!'
+        return jsonify({'result': output})
+    return render_template('deploy.html')
